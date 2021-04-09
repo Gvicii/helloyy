8f372198bf6476af0d23fd68c457598bca3da602
Suzukaze <60780980+IJN-Suzukaze@users.noreply.github.com>
2021-04-09 12:43:57 +0900

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("这是我的第一个git上传的项目 ");
         System.out.println("这是我项目的尊贵的第二个版本 ");
+        System.out.println("合作者提交测试 ");
     }
 }
